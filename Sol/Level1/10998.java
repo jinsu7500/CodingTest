420d9d92499bac494e735bd21d358b9aa07834b8
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2022-04-18 16:52:40 +0900

    Create 10998.java

---
+++ Sol/Level1/10998.java
@@ -0,0 +1,28 @@
+/************************************************************************************************/
+/* 10998번 : A * B
+/************************************************************************************************/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        //BufferReader 선언
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //string 읽기
+        String str = br.readLine();
+
+        //공백 분리
+        StringTokenizer st = new StringTokenizer(str," ");
+
+        //입력선언
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        //결과 출력
+        System.out.println(a*b);
+    }
+}
