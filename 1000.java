f10ac926501fd230b770dc2a10005f4afd03c5f3
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2022-04-18 13:39:31 +0900

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,15 @@
+/************************************************************************************************/
+/* 1000번 : A+B
+/************************************************************************************************/
+/*import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //1000 번       A+B
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+
+    }
+}
+*/
