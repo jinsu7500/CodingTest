2bedf80e125b563a37eb31a23e4806ede75354f2
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2022-04-19 11:05:13 +0900

    Create 10926.java

---
+++ Sol/Level1/10926.java
@@ -0,0 +1,20 @@
+/************************************************************************************************/
+/* 10926번 : ??!
+/************************************************************************************************/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        //BufferReader 선언
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //string 읽기
+        String str = br.readLine();
+
+        //결과 출력
+        System.out.println(str + "??!");    
+    }
+}
