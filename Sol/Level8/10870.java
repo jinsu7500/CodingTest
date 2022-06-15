3202e7c2aeeb34ddad1b3eeebe64e40428327b9a
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2022-06-15 09:57:44 +0900

    Create 10870.java

---
+++ Sol/Level8/10870.java
@@ -0,0 +1,29 @@
+/************************************************************************************************/
+/* 10870번 : 피보나치 수 5
+/************************************************************************************************/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {			
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(bfr.readLine());
+		System.out.println(fibonacci(N));
+		
+
+		
+	}
+
+	public static int fibonacci(int N)
+	{		
+		if(N <= 1 )
+			return N;
+		else
+		{
+			return fibonacci(N-1) + fibonacci(N-2);
+		}
+	}
+}
