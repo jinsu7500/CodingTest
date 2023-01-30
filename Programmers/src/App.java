1f957ae514128d323dacad6bf2f1fc2913e3caad
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-01-31 00:46:22 +0900

    VSCODE 디렉토리변경
    VSCODE 디렉토리변경

--- Programmers/src/App.java
+++ Programmers/src/App.java
@@ -2,30 +2,15 @@
 import java.util.Arrays;
 
 import LV2.Carpet;
+import LV2.ExpectedBracket;
 import LV2.Printer;
 import LV2.nextMaxNumber;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {       
-        
-        
-
-        // nextMaxNumber test = new nextMaxNumber();
-        // System.out.println(test.solution(78));
-        // System.out.println(test.solution(15));    
-        
-        Printer test2 = new Printer();
-
-        int testcase[] = {1,1,9,1,1,1};
-        // int testcase[] = {2,1,3,2};
-
-         System.out.println(test2.solution(testcase, 2));
-         System.out.println(test2.solution(testcase, 2));
-
-
-
-        
+    public static void main(String[] args) throws Exception {     
+         ExpectedBracket test = new ExpectedBracket();
+         System.out.println(test.solution2(8, 7, 4));        
     }
 }
 
