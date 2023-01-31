45407d7c6f996ced450586428604a9e27b814507
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-01-31 21:17:48 +0900

    01.31 멀리뛰기 추가

--- Programmers/src/App.java
+++ Programmers/src/App.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 import LV2.Carpet;
 import LV2.ExpectedBracket;
 import LV2.Printer;
+import LV2.longJump;
 import LV2.nextMaxNumber;
 
 
 public class App {
     public static void main(String[] args) throws Exception {     
-         ExpectedBracket test = new ExpectedBracket();
-         System.out.println(test.solution2(8, 7, 4));        
+         longJump test = new longJump();
+         System.out.println(test.solution(4));
+         System.out.println(test.solution(3));
+
     }
 }
 
