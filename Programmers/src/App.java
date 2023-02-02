13555842702d10088eb8696093f636ac16402501
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-02-03 00:10:04 +0900

    23.02.03 백준
    5086 : 배수와 약수

--- Programmers/src/App.java
+++ Programmers/src/App.java
@@ -1,20 +1,34 @@
-
-import java.util.Arrays;
-
-import LV2.Carpet;
-import LV2.ExpectedBracket;
-import LV2.Printer;
-import LV2.longJump;
-import LV2.nextMaxNumber;
-
-
+import java.io.*;
+import java.util.StringTokenizer;
 public class App {
-    public static void main(String[] args) throws Exception {     
-         longJump test = new longJump();
-         System.out.println(test.solution(4));
-         System.out.println(test.solution(3));
+    public static void main(String[] args) throws Exception {  
+        // //BufferReader,BufferWriter 선언
+        // BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));   
 
+        // //Stringtokenizer
+        
+        // while(true){
+        //     StringTokenizer st = new StringTokenizer(bfr.readLine());
+        //     int firstNum = Integer.parseInt(st.nextToken(" "));
+        //     int secondNum = Integer.parseInt(st.nextToken(" "));
+        //     String answer = " ";
+            
+        //     if(firstNum == 0 && secondNum == 0) break;
+
+        //     //factor
+        //     if(secondNum % firstNum == 0)
+        //         answer = "factor";
+        //     //multiple
+        //     else if(firstNum % secondNum == 0 )
+        //         answer = "multiple";
+        //     // if((secondNum % firstNum == 1) ||(firstNum % secondNum == 1))
+        //     else
+        //         answer = "neither";
+                
+        //     bfw.write(answer + "\n");
+        //     bfw.flush();            
+        // }
+        // bfw.close();
     }
 }
-
-
