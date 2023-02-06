85a2bb5cedb37c33f3797858ccdafddadfe4ded7
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-02-07 00:15:19 +0900

    02.07 백준
    02.07/ 백준 / 1037 : 약수

---
+++ BaekJoon/Level15/1037.java
@@ -0,0 +1,62 @@
+/************************************************************************************************/
+/* 1037번 : 약수
+/************************************************************************************************/
+/*
+ * 문제
+양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N의 진짜 약수의 개수가 주어진다. 이 개수는 50보다 작거나 같은 자연수이다. 둘째 줄에는 N의 진짜 약수가 주어진다. 1,000,000보다 작거나 같고, 2보다 크거나 같은 자연수이고, 중복되지 않는다.
+
+출력
+첫째 줄에 N을 출력한다. N은 항상 32비트 부호있는 정수로 표현할 수 있다.
+
+예제 입력 1 
+2
+4 2
+예제 출력 1 
+8
+예제 입력 2 
+1
+2
+예제 출력 2 
+4
+예제 입력 3 
+6
+3 4 2 12 6 8
+예제 출력 3 
+24
+예제 입력 4 
+14
+14 26456 2 28 13228 3307 7 23149 8 6614 46298 56 4 92596
+예제 출력 4 
+185192
+ */
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {  
+        //BufferReader,BufferWriter 선언
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));        
+
+        //Stringtokenizer
+        int count = Integer.parseInt(bfr.readLine());
+        int arr[] = new int[count];
+        StringTokenizer st = new StringTokenizer(bfr.readLine());
+
+        //input
+        for(int i = 0 ; i<count ; i++){
+            arr[i] = Integer.parseInt(st.nextToken(" "));
+        }
+
+        //sort & output
+        Arrays.sort(arr);
+        int max = arr[0];
+        int min = arr[count-1];
+        System.out.println(max*min);
+    }
+}
--- Programmers/src/App.java
+++ Programmers/src/App.java
@@ -1,43 +1,27 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-import LV2.Spy;
+
 public class App {
     public static void main(String[] args) throws Exception {  
-        // //BufferReader,BufferWriter 선언
-        // BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-        // BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));   
+        //BufferReader,BufferWriter 선언
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));        
 
-        // //Stringtokenizer
-        
-        // while(true){
-        //     StringTokenizer st = new StringTokenizer(bfr.readLine());
-        //     int firstNum = Integer.parseInt(st.nextToken(" "));
-        //     int secondNum = Integer.parseInt(st.nextToken(" "));
-        //     String answer = " ";
-            
-        //     if(firstNum == 0 && secondNum == 0) break;
+        //Stringtokenizer
+        int count = Integer.parseInt(bfr.readLine());
+        int arr[] = new int[count];
+        StringTokenizer st = new StringTokenizer(bfr.readLine());
 
-        //     //factor
-        //     if(secondNum % firstNum == 0)
-        //         answer = "factor";
-        //     //multiple
-        //     else if(firstNum % secondNum == 0 )
-        //         answer = "multiple";
-        //     // if((secondNum % firstNum == 1) ||(firstNum % secondNum == 1))
-        //     else
-        //         answer = "neither";
-                
-        //     bfw.write(answer + "\n");
-        //     bfw.flush();            
-        // }
-        // bfw.close();
+        //input
+        for(int i = 0 ; i<count ; i++){
+            arr[i] = Integer.parseInt(st.nextToken(" "));
+        }
 
-        Spy test = new Spy();
-        String[][] testCase = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        String[][] testCase2 = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
-
-        System.out.println(test.solution(testCase));
-        System.out.println(test.solution(testCase2));
+        //sort & output
+        Arrays.sort(arr);
+        int max = arr[0];
+        int min = arr[count-1];
+        System.out.println(max*min);
     }
 }
