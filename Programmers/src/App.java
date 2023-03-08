a702f176376b7effc12fd13947edf3a73fef79cf
JinsuPark <bluesky3213489@gmail.com>
2023-03-09 00:10:02 +0900

    프로그래머스 택배상자 LV2
    프로그래머스 택배상자 LV2

--- Programmers/src/App.java
+++ Programmers/src/App.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import LV2.ContinuousSeq;
+import LV2.DeliveryBox;
 import LV2.H_Index;
 import LV2.HotelReservation;
 import LV2.NumberCardDiv;
@@ -29,18 +30,13 @@ public class App {
         // int max = arr[0];
         // int min = arr[count-1];
         // System.out.println(max*min);
-
-        HotelReservation test = new HotelReservation();                           
-        String testcase[][] = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
-        String testcase2[][] = {{"09:10", "10:10"},{"10:20","12:20"}};
-        String testcase3[][] = {{"10:20", "12:30"}, {"10:20", "12:30"}, {"10:20", "12:30"}};
-        String testcase4[][] = {{"16:00", "16:10"},{"16:20","16:30"},{"16:53","16:55"}};
         
+        DeliveryBox test = new DeliveryBox();
+        int[] testcaset = {4,3,1,2,5};
+        int[] testcaset2 = {5,4,3,2,1};
 
+        System.out.println(test.solution(testcaset));
+        System.out.println(test.solution(testcaset2));
 
-        // System.out.println(test.solution(testcase));
-        // System.out.println(test.solution(testcase2));
-        // System.out.println(test.solution(testcase3));
-        System.out.println(test.solution(testcase4));
     }
 }
