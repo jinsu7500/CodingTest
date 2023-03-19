59e59f5981bb50cad1f4e52603f284890ff0d7ef
JinsuPark <bluesky3213489@gmail.com>
2023-03-20 02:06:08 +0900

    2023.03.20 / 영어 끝말잇기

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,18 +1,36 @@
 import java.io.*;
 import java.util.*;
 
+import LV2.EnglishFollowUp;
+
 public class Main {
     public static void main(String[] args) throws IOException {  
-        //BufferReader,BufferWriter 선언
-        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));       
+        // //BufferReader 선언
+        // BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));  
+        // StringTokenizer st = new StringTokenizer(bfr.readLine()," ");
 
-        //Stringtokenizer        
-        StringTokenizer st = new StringTokenizer(bfr.readLine());
+        // int n = Integer.parseInt(st.nextToken());
+        // int m = Integer.parseInt(st.nextToken());
 
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
+        // //arr input
+        // int[] arr = new int[n];
+        // for(int i = 0 ; i<n ; i++){
+        //     arr[0] = i+1;
+        // }
 
-        System.out.print(A+B+C);
+        // //{1,2,3}
+
+        EnglishFollowUp test = new EnglishFollowUp();
+        String[] testCase = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        String[] testCase2 = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+        String[] testCase3 = {"hello", "one", "even", "never", "now", "world", "draw"};
+
+        // int[] tt = test.solution(3, testCase);
+        int[] tt = test.solution(5, testCase2);
+        // int[] tt = test.solution(2, testCase3);
+        
+        System.out.println(tt[0] + "," + tt[1]);
+    
+        
     }
 }
