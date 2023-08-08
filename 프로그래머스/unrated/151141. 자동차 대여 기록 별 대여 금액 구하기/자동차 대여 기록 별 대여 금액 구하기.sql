8be37bed9648092ba371affc1eb12c85395f9a38
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-08-08 18:23:52 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,19 +1,24 @@
-SELECT HISTORY_ID, 
-    round(DAILY_FEE * (DATEDIFF(h.END_DATE,h.START_DATE)+1)
-    * (case 
-    when DATEDIFF(END_DATE,START_DATE)+1 < 7 then 1
-    when DATEDIFF(END_DATE,START_DATE)+1 < 30 then 0.95
-    when DATEDIFF(END_DATE,START_DATE)+1 < 90 then 0.92
-    else 0.85 end)) as "FEE"
-
-from CAR_RENTAL_COMPANY_CAR as c 
-    join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-    on c.CAR_ID = h.CAR_ID
-    join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-    on c.CAR_TYPE = p.CAR_TYPE
-
-where c.car_type = "트럭"
-
-group by HISTORY_ID
-
-order by FEE desc , HISTORY_ID desc
+SELECT A.HISTORY_ID
+     , ROUND(A.DAILY_FEE * A.RENTDATE * (100 - NVL(C.DISCOUNT_RATE, 0)) / 100) AS FEE
+  FROM 
+    ( SELECT A.CAR_TYPE
+           , A.DAILY_FEE
+           , B.HISTORY_ID             
+           , (B.END_DATE - B.START_DATE ) +1 AS RENTDATE
+           , CASE WHEN (B.END_DATE - B.START_DATE) + 1 < 7 THEN '할인없음'
+                  WHEN (B.END_DATE - B.START_DATE) + 1 < 30 THEN '7일 이상'
+                  WHEN (B.END_DATE - B.START_DATE) + 1 < 90 THEN '30일 이상'
+                  ELSE '90일 이상'
+                   END AS DURATION_TYPE                    
+          FROM CAR_RENTAL_COMPANY_CAR A  
+             , CAR_RENTAL_COMPANY_RENTAL_HISTORY B             
+         WHERE 1=1 
+          AND A.CAR_ID = B.CAR_ID  
+          AND A.CAR_TYPE = '트럭'          
+  ) A
+  , CAR_RENTAL_COMPANY_DISCOUNT_PLAN C
+WHERE 1=1
+ AND A.CAR_TYPE = C.CAR_TYPE(+)
+ AND A.DURATION_TYPE = C.DURATION_TYPE (+)
+ORDER BY FEE DESC
+       , HISTORY_ID DESC
