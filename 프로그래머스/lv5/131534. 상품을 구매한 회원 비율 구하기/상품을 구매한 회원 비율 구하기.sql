512ba7c689cc265993841efa5770af048cf22ad0
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-08-10 21:03:41 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,23 +1,21 @@
-WITH USER_2021 AS (
-    SELECT A.* 
-      FROM USER_INFO A
-    WHERE 1=1
-      AND TO_CHAR(A.JOINED,'YYYY') = '2021'    
-)
-
-
 SELECT TO_CHAR(B.SALES_DATE,'YYYY')             AS YEAR
      , TO_NUMBER(TO_CHAR(B.SALES_DATE,'MM'  ))  AS MONTH
      , COUNT(DISTINCT A.USER_ID) AS PUCHASED_USERS
      , ROUND(COUNT(DISTINCT A.USER_ID) / 
-             (SELECT COUNT(*) FROM USER_2021) , 1
-            ) AS RATIO
+             (SELECT COUNT(*) FROM USER_INFO 
+                             WHERE 1=1
+                               AND TO_CHAR(A.JOINED,'YYYY') = '2021' ) , 1
+            ) AS PUCHASED_RATIO
      
- FROM USER_2021 A
+ FROM ( SELECT A.* 
+      FROM USER_INFO A
+    WHERE 1=1
+      AND TO_CHAR(A.JOINED,'YYYY') = '2021'
+      ) A
     , ONLINE_SALE B    
 WHERE 1=1
   AND A.USER_ID = B.USER_ID
 GROUP BY TO_CHAR(B.SALES_DATE,'YYYY') 
        , TO_CHAR(B.SALES_DATE,'MM'  )
-ORDER BY YEAR
-       , MONTH
+ORDER BY 2
+       , 1
