2198575fc3510e19e14ba6bf9abf56090bba780e
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-08-17 20:44:46 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,33 +1,15 @@
-  SELECT Z.MEMBER_NAME                                AS MEMBER_NAME
-       , M.REVIEW_TEXT                                AS REVIEW_TEXT
-       , TO_CHAR(M.REVIEW_DATE,'YYYY-MM-DD')          AS REVIEW_DATE       
-    FROM REST_REVIEW M       
-       , (
-         SELECT X.CNT
-              , X.MEMBER_ID
-           FROM
-              (
-                SELECT COUNT(A.MEMBER_ID)   AS CNT
-                     , A.MEMBER_ID
-                  FROM REST_REVIEW A
-                 WHERE 1=1  
-                  GROUP BY A.MEMBER_ID                
-              ) X
-             , (
-                SELECT MAX(COUNT(A.MEMBER_ID)) AS MAX_CNT
-                  FROM REST_REVIEW A
-                 GROUP BY A.MEMBER_ID
-               ) Y
-         WHERE 1=1
-           AND X.CNT = Y.MAX_CNT
-         ) N 
-      , MEMBER_PROFILE Z
-  WHERE 1=1
-   AND M.MEMBER_ID = N.MEMBER_ID
-   AND M.MEMBER_ID = Z.MEMBER_ID
- ORDER BY REVIEW_DATE ASC
-        , REVIEW_TEXT ASC
-
-  
-  
-  
+SELECT a.member_name
+     , b.review_text
+     , to_char(b.review_date, 'YYYY-MM-DD') AS review_date
+  from member_profile a
+,rest_review b
+    
+ where 1=1
+  and  a.member_id = b.member_id
+  and a.member_id in (select member_id
+                         from rest_review
+                        group by member_id
+                        having count(*) = (select max(count(*))
+                                             from rest_review
+                                            group by member_id))
+order by 3, 2
