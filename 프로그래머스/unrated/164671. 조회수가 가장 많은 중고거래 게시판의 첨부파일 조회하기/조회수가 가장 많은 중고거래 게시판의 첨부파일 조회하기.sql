06262538ed1262f966042bd539b66407196fbb8b
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2023-09-12 22:34:10 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,23 @@
+SELECT '/home/grep/src/'||A.BOARD_ID||'/'||A.FILE_ID||A.FILE_NAME||A.FILE_EXT
+         AS FILE_PATH
+  FROM USED_GOODS_FILE A
+     , (
+        SELECT *
+          FROM (
+                SELECT MAX(VIEWS) AS MAX_VIEWS
+                     , BOARD_ID
+                  FROM USED_GOODS_BOARD 
+                  GROUP BY BOARD_ID
+                  ORDER BY 1 DESC
+               )
+        WHERE 1=1
+          AND ROWNUM < 2
+       ) B
+ WHERE 1=1
+   AND A.BOARD_ID = B.BOARD_ID
+
+ ORDER BY A.FILE_ID DESC
+
+
+
+  
