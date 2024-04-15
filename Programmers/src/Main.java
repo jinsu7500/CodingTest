5e478cb72b4fad3ffdd6b2d2ca74dad8a4ddc448
JinsuPark <bluesky3213489@gmail.com>
2024-04-16 00:35:58 +0900

    2024.04.16

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -8,36 +8,97 @@ public class Main {
     public static void main(String[] args) throws IOException {  
         // //BufferReader 선언
         BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));  
-        // BufferedWriter bwr = new BufferedWriter(new OutputStreamWriter(System.out));
+        // // BufferedWriter bwr = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int size = Integer.parseInt(bfr.readLine());       
-        
+        String output  = "";
+        boolean bYN = true;
 
-        for(int i = 0 ; i < size ; i++)
+        for(;;)
         {
             Stack<Character> stack = new Stack<>();
             String inputText = bfr.readLine();
-            boolean bYN = true;
-
-            for(int j = 0 ; j <inputText.length() ; j++)
+            if(inputText == ".") { break; }
+            
+            for(int i = 0 ; i < inputText.length(); i++)
             {
-                if(inputText.charAt(j) == '(')
+                
+
+                char chk = inputText.charAt(i);
+
+                if(chk == '(' || chk == '[')
                 {
-                    stack.push(inputText.charAt(j));
+                    stack.push(chk);
                 }
-                else if(stack.isEmpty())
+                else if (chk == ')')
                 {
-                    bYN = false;
-                    break;
+                    if(stack.isEmpty() || stack.peek() != '(')
+                    {
+                        output = "no";
+                        bYN = false;
+                        System.out.println(output);
+                        break;
+                    }
+                    else
+                    {
+                        stack.pop();
+                    }
                 }
-                else
+                else if (chk == ']')
                 {
-                    stack.pop();
+                    if(stack.isEmpty() || stack.peek() != '[')
+                    {                        
+                        output = "no";
+                        bYN = false;
+                        System.out.println(output);
+                        break;
+                    }
+                    else
+                    {
+                        stack.pop();
+                    }
                 }
+
             }
-            System.out.println(bYN && stack.isEmpty() ? "YES" :  "NO");
+
+            if(stack.isEmpty() &&  bYN == true)
+            {
+                output = "yes";
+                break;
+            }
+
+            
+            
+        }
+        System.out.println(output);
+        
+        // int size = Integer.parseInt(bfr.readLine());       
+        
+
+        // for(int i = 0 ; i < size ; i++)
+        // {
+        //     Stack<Character> stack = new Stack<>();
+        //     String inputText = bfr.readLine();
+        //     boolean bYN = true;
+
+        //     for(int j = 0 ; j <inputText.length() ; j++)
+        //     {
+        //         if(inputText.charAt(j) == '(')
+        //         {
+        //             stack.push(inputText.charAt(j));
+        //         }
+        //         else if(stack.isEmpty())
+        //         {
+        //             bYN = false;
+        //             break;
+        //         }
+        //         else
+        //         {
+        //             stack.pop();
+        //         }
+        //     }
+        //     System.out.println(bYN && stack.isEmpty() ? "YES" :  "NO");
                
-        } 
+        // } 
         bfr.close();
     }
 }
