6dc632f743980a45d454e75709ba274448bdab55
JinsuPark <56360477+jinsu7500@users.noreply.github.com>
2024-08-26 23:48:28 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for(int list : num_list)
+        {
+            if(list % 2 == 0)
+            {
+                answer[0] += 1;
+            }
+            else
+            {
+                answer[1] += 1;
+            }
+        }
+        return answer;
+    }
+}
